d6283ffb2dd80d4294eb374627cc0faaf0a06dd7
Swaroop Akkineni <swa7@pitt.edu>
2019-03-13 16:30:18 -0400

    initial setup

---
+++ website/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <mySite/>
+  </div>
+</template>
+
+<script>
+import mySite from './components/mySite.vue';
+
+export default {
+  name: 'app',
+  components: {
+    mySite,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  background: linear-gradient(45deg, #17ead9, #6078ea);
+  height: 100vh;
+  margin: 0;
+  background-attachment: fixed;
+}
+</style>
---
+++ website/src/components/mySite.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mySite">
+    <p>Swaroop Akkineni</p>
+    <p>Software Engineer. Runner. Gamer.</p>
+    <a href="https://www.linkedin.com/in/swaroop-akkineni">
+      <img src="../assets/linkedin.png">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+};
+
+</script>
+
+<style>
+  .mySite {
+    align: left;
+    /* margin-top: 10%; */
+    margin-left: 5%;
+  }
+
+  .image {
+    background-repeat: no-repeat;
+    background-position: 50%;
+    border-radius: 50%;
+    width: 200px;
+    height: 200px;
+  }
+</style>
